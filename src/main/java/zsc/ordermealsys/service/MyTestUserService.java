1bd66747a4188dad5785ffa260e5c37afe8ce6cf
xuedie12369 <501968797@qq.com>
2018-03-18 16:38:56 +0800

    第一次和hjs合并项目
    完成了用户模块的功能开发

--- src/main/java/zsc/ordermealsys/service/MyTestUserService.java
+++ src/main/java/zsc/ordermealsys/service/MyTestUserService.java
@@ -8,9 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
 import zsc.ordermealsys.common.ServerResponse;
-import zsc.ordermealsys.controller.UserController;
 import zsc.ordermealsys.dao.UserMapper;
-import zsc.ordermealsys.impl.UserServiceImpl;
 import zsc.ordermealsys.pojo.User;
 /*误删*/
 /*误删*/
@@ -40,14 +38,12 @@ public class MyTestUserService {
 	public  void main() {
 		// TODO Auto-generated method stub
 		ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
-		UserServiceImpl u= ac.getBean(UserServiceImpl.class);
+		MyTestUserService u= ac.getBean(MyTestUserService.class);
 		/*User us=new User();
 		us.setUserName("dm");
 		us.setEmail("007");*/
-		u.login("shn", "1234");
-		//u.register(us);
-	
-		/*System.out.print(userDao.checkUsername("shn"));*/
+		int n=u.userDao.checkUsername("shn");
+		//u.register(us);t
+		System.out.print(n);
 	}
-
 }
