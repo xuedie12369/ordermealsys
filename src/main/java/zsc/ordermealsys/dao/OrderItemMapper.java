d19d66c528359be4ab9c9070c888a733294a648e
xuedie12369 <501968797@qq.com>
2018-03-25 10:07:26 +0800

    修改了OrderItem实体类和Order实体类
    1、修改买家user_name字段为user_id
    2、修改订单order_id字段为order_no
    3、增加了查询订单项目的函数和查询订单表的功能

--- src/main/java/zsc/ordermealsys/dao/OrderItemMapper.java
+++ src/main/java/zsc/ordermealsys/dao/OrderItemMapper.java
@@ -1,5 +1,9 @@
 package zsc.ordermealsys.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import zsc.ordermealsys.pojo.OrderItem;
 import zsc.ordermealsys.pojo.OrderItemWithBLOBs;
 
@@ -17,4 +21,7 @@ public interface OrderItemMapper {
     int updateByPrimaryKeyWithBLOBs(OrderItemWithBLOBs record);
 
     int updateByPrimaryKey(OrderItem record);
+    
+    //自写部分
+    List<OrderItemWithBLOBs> getByOrderNoAndUserId(@Param("orderNo")Long orderNo,@Param("userId")Integer userId);
 }
